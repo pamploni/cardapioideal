e02ac6a050596cc26859839236a4940943d95a90
Pamploni Cadé <pamplonicade@MacBook-Air-de-Pamploni.local>
2020-10-12 09:47:30 -0300

    atualização preço cardápio marujobar

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.18:5555',
+  baseURL: 'http://localhost:5555',
 });
 
 export default api;
--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.18:3000',
+  baseURL: 'http://localhost:3000',
 });
 
 export default api;
+git;
